60e7d3c4496af0338ccd9a8a888dbeba9f0a65c1
amine <amine.yaker@opensee.io>
2023-01-04 00:14:05 +0100

    finished chapter 2 the GET Monad

--- typeclasses-chapter2-exercice/common-typeclasses-exercises-main/src/main/scala/fpcourse/Get.scala
+++ typeclasses-chapter2-exercice/common-typeclasses-exercises-main/src/main/scala/fpcourse/Get.scala
@@ -23,20 +23,24 @@ object Get {
   /**
    * TODO 1
    * Consumes n bytes of input parsing no value.
+   * 
    */
-  def skip(n: Int): Get[Unit] = ???
+  def skip(n: Int): Get[Unit] =  Get[Unit](list => if (list.size < n) Left("input too small to skip") else Right(list.drop(n), ()))
 
   /**
    * TODO 2
    * True if the input is fully consumed
    */
-  def isEmpty: Get[Boolean] = ???
+  def isEmpty: Get[Boolean] = Get(list => Right(list, list.isEmpty))
 
   /**
    * TODO 3
    * Reads one byte from input
    */
-  def getByte: Get[Byte] = ???
+  def getByte: Get[Byte] = Get[Byte](bytes => bytes match {
+    case head :: tl => Right(tl, head)
+    case Nil => Left("empty input")
+  })
 
   /**
    * TODO 4
@@ -44,21 +48,24 @@ object Get {
    *
    * Hint: Consider using the method replicateA in Applicative.
    */
-  def getIntBE: Get[Int] = ???
-
+  def getIntBE: Get[Int] = getByte.replicateA(4).map { bytes =>
+    bytesToIntUnsafe(bytes.toArray, ByteOrder.BIG_ENDIAN)
+  }
   /**
    * TODO 5
    * Reads an Int from input using Little Endian order.
    *
    * Hint: Consider using the method replicateA in Applicative.
    */
-  def getIntLE: Get[Int] = ???
+  def getIntLE: Get[Int] = getByte.replicateA(4).map { bytes =>
+    bytesToIntUnsafe(bytes.toArray, ByteOrder.LITTLE_ENDIAN)
+  }
 
   /**
    * TODO 6
    * Reads a String of n characters from input.
    */
-  def getString(n: Int): Get[String] = ???
+  def getString(n: Int): Get[String] = getByte.replicateA(n).map( bytes => new String(bytes.toArray))
 
   /**
    * Helper function that turns four bytes into an Int. It doesn't check the
@@ -76,9 +83,15 @@ object Get {
    * Instance of monad error for Get.
    */
   implicit val monadGet: MonadError[Get, String] = new MonadError[Get, String] {
-    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] = ???
-
-    override def pure[A](x: A): Get[A] = ???
+    override def flatMap[A, B](fa: Get[A])(f: A => Get[B]): Get[B] = Get { bytes =>
+      fa.run(bytes) match {
+        case Right((remainingBytes, a)) => f(a).run(remainingBytes)
+        case Left(e) => Left(e)
+      }
+    }
+    override def pure[A](x: A): Get[A] = Get { bytes =>
+      Right((bytes, x))
+    }
 
     override def tailRecM[A, B](a: A)(f: A => Get[Either[A, B]]): Get[B] = {
       Get { bytes =>
@@ -93,9 +106,16 @@ object Get {
       }
     }
 
-    override def raiseError[A](e: String): Get[A] = ???
+    override def raiseError[A](e: String): Get[A] = Get { _ => Left(e)}
 
-    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] = ???
+    override def handleErrorWith[A](fa: Get[A])(f: String => Get[A]): Get[A] = Get { bytes =>
+      fa.run(bytes) match {
+        case r @ Right(_) => r
+        case Left(e) => f(e).run(bytes)
+      }
+    }
+  
+    
   }
 
   /**
@@ -108,11 +128,41 @@ object Get {
    * a generator of List[Byte], then sample it several times (e.g. 32)
    * and check that running both Gets yields the same result every time.
    */
-  implicit def eqGet[A: Eq]: Eq[Get[A]] = ???
+  implicit def eqGet[A: Eq]: Eq[Get[A]] = Eq.instance { (g1, g2) =>
+    val nSamples = 32
+    val genBytes = Gen.listOf(Gen.choose(0, 255).map(_.toByte))
+    Iterator
+      .continually(genBytes.sample)
+      .take(nSamples)
+      .flatten
+      .forall(bs => g1.run(bs) === g2.run(bs))
+  }
 
   /**
    * TODO 9
    * Monoid instance for Get.
    */
-  implicit def monoid[A: Monoid]: Monoid[Get[A]] = ???
+  implicit def monoid[A: Monoid]: Monoid[Get[A]] =  new Monoid[Get[A]] {
+    /**
+     * Read the docs for combine and come up with an instance that does not
+     * alter the behaviour of any Get it is combined with.
+     *
+     * Think about what should happen to the input bytes, and what would be a
+     * suitable result.
+     */
+    override def empty: Get[A] = Monad[Get].pure(Monoid[A].empty)
+
+    /**
+     * Combining two Get[A] instances should yield a new Get[A] instance which
+     * runs both Gets in sequence and yields the combined result.
+     *
+     * If any of the Gets fails, the combined Get should fail with that same error.
+     *
+     * Check the tests for details.
+     */
+    override def combine(x: Get[A], y: Get[A]): Get[A] =
+      (x, y).mapN((a1, a2) => a1 |+| a2)
+  }
 }
+
+
